e1af498e29d86a9e88f98646b89b3956d9bc77d5
martin lowe <martinlowe673@gmail.com>
2021-10-16 18:47:44 +0100

    added light

--- src/LightSwitch.js
+++ src/LightSwitch.js
@@ -1,26 +1,23 @@
-import React from "react"
-import styled from 'styled-componets';
+import React from "react";
+import styled from "styled-components";
 
-const Button = styled.button `
-position: absolute;
-left: ${props => (props.postion === 'left ?' '20px' : '380px')};
-bottom: 20px;
-background: white;
-color: black;
-border: 1px solid black;
-border-radius: 10px;
-width: 100px;
-height: 50px;
-cursor: PointerEvent;
+const Button = styled.button`
+  position: absolute;
+  left: ${(props) => (props.position === "left" ? "20px" : "380px")};
+  bottom: 20px;
+  background: white;
+  color: black;
+  border: 1px solid black;
+  border-radius: 10px;
+  width: 100px;
+  height: 50px;
+  cursor: Pointer;
+`;
 
-
-`
-
-
-const LightSwitch = ({callback, postion switchOn}) => (
-  <Button onClick={callback} postion={postion}>
-  {switchOn ? 'On' : 'Off'}
+const LightSwitch = ({ callback, position, switchOn }) => (
+  <Button onClick={callback} position={position}>
+    {switchOn ? "On" : "Off"}
   </Button>
-)
+);
 
 export default LightSwitch;
